3fcfb77accfaad94b245bd921c9f2e4ee211d5f9
unknown <kristinamisenko9@gmail.com>
2022-08-06 18:23:16 +0300

    Изменен дизайн маркеров на карте

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,8 +1,8 @@
 const TIMEOUT_SHOW_ERROR = 2000;
 
-const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+const URL_MARKER_DEFAULT = 'img/pin.svg';
 
-const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+const URL_MARKER_CURRENT = 'img/pin-active.svg';
 
 enum AppRoute {
     Main = '/',
